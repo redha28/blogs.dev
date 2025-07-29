ccb69ff03228f9a2ca67881649f1de9a3962aaca
redha28 <redhadefinto28@gmail.com>
2025-07-29 23:50:59 +0700

    [feat]: implement main application structure with routing and state management

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { store } from "./store/store";
+import HomePage from "./pages/HomePage";
+import ArticlePage from "./pages/ArticlePage";
+import NotFound from "./pages/NotFound";
+import ScrollToTop from "./hooks/useScrollToTop";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/article/:id" element={<ArticlePage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </Provider>
+);
+
+export default App;
